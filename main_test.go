fe74ad712553566ebad28ac3d079c3f4af96b817
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2024-08-07 13:00:13 +0300

    writing the go tests

--- main_test.go
+++ main_test.go
@@ -10,6 +10,7 @@ import (
 	"net/http/httptest"
 	"encoding/json"
 	"bytes"
+	"strconv"
 )
 
 var a main.App
@@ -101,6 +102,29 @@ func TestCreateProduct(t *testing.T) {
 	// when the target is a map[string]interface{}
 	if m["id"] != 1.0 {
 		t.Errorf("Expected product ID to be '1'. Got '%v'", m["id"])
+	}
+}
+
+// Adds a product to table and checks if accessing it results in success response.
+func TestGetProduct(t *testing.T) {
+	clearTable()
+
+	addProducts(1)
+
+	req, _ := http.NewRequest("GET", "/product/1", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+}
+
+// The function to add one or more records into the table for testing.
+func addProducts(count int) {
+	if count < 1 {
+		count = 1
+	}
+
+	for i := 0; i < count; i++ {
+		a.DB.Exec("INSERT INTO products(name, price) VALUES($1, $2)", "Product "+strconv.Itoa(i),  (i+1.0)*10)
 	}
 }
 
