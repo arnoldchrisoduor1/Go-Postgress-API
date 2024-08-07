3bc36b068816a8849f51170913f3c4a24f1038e8
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2024-08-07 14:18:44 +0300

    Function to handle getting a single product

--- app.go
+++ app.go
@@ -5,6 +5,9 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net/http"
+	"strconv"
+	"encoding/json"
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
@@ -25,5 +28,46 @@ func (a *App) Initialize() {
 	}
 	a.Router = mux.NewRouter()
  }
+
+// Creating a handler for the route that fetches a single product.
+
+func (a *App) getProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid product ID")
+		return
+	}
+
+	p := product{ID: id}
+	if err := p.getProduct(a.DB); err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			respondWithError(w, http.StatusNotFound, "Product not found")
+		default:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+	responseWithJSON(w, http.StatusOK, p)
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	responseWithJSON(w, code, map[string]string{"error":message})
+}
+
+func responseWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+// A handler to fetch a list of products.
+func (a *App) getProducts(w http.ResponseWriter, r *http.Request) {
+	count, _ := strconv.Atoi(r.FormValue("count"))
+	start, _ := strconv.Atoi(r.FormValue("start"))
+}
 
 func (a *App) Run(addr string) { }
